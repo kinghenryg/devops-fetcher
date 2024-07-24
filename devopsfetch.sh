e5ec4565f2e16f98b61db38eae305415742e873b
chimsom <okorogladys52@gmail.com>
2024-07-24 21:42:44 +0100

    fix tables

--- devopsfetch.sh
+++ devopsfetch.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 
-#!/usr/bin/env bash
-
-####
-# Copyright (c) 2016-2021
-#   Jakob Westhoff <jakob@westhoffswelt.de>
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#  - Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer.
-#  - Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUsed AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVIsed OF THE POSSIBILITY OF SUCH DAMAGE.
-####
+# Prettytable code
 
 _prettytable_char_top_left="┌"
 _prettytable_char_horizontal="─"
@@ -39,28 +14,6 @@ _prettytable_char_vertical_horizontal_top="┬"
 _prettytable_char_vertical_horizontal_bottom="┴"
 _prettytable_char_vertical_horizontal="┼"
 
-
-# Escape codes
-
-# Default colors
-_prettytable_color_blue="0;34"
-_prettytable_color_green="0;32"
-_prettytable_color_cyan="0;36"
-_prettytable_color_red="0;31"
-_prettytable_color_purple="0;35"
-_prettytable_color_yellow="0;33"
-_prettytable_color_gray="1;30"
-_prettytable_color_light_blue="1;34"
-_prettytable_color_light_green="1;32"
-_prettytable_color_light_cyan="1;36"
-_prettytable_color_light_red="1;31"
-_prettytable_color_light_purple="1;35"
-_prettytable_color_light_yellow="1;33"
-_prettytable_color_light_gray="0;37"
-
-# Somewhat special colors
-_prettytable_color_black="0;30"
-_prettytable_color_white="1;37"
 _prettytable_color_none="0"
 
 function _prettytable_prettify_lines() {
@@ -113,10 +66,6 @@ function prettytable() {
     } | column -t -s $'\t' | _prettytable_fix_border_lines | _prettytable_colorize_lines "${color}" "2"
 }
 
-if [ "$0" = "$BASH_SOURCE" ]; then
-    # Execute function if called as a script instead of being sourced.
-    prettytable $*
-fi
 
 LOG_FILE="/var/log/devopsfetch.log"
 
@@ -130,53 +79,55 @@ log() {
 }
 
 display_active_ports() {
-    sudo netstat -tulpn | grep LISTEN
+    echo -e "Proto\tLocal Address\tForeign Address\tState\tPID/Program name" | cat - <(sudo netstat -tulpn | grep LISTEN | awk '{print $1"\t"$4"\t"$5"\t"$6"\t"$7}') | prettytable 5 cyan
 }
 
 get_port_info() {
-    ss -tuln sport = ":$1"
+    echo -e "State\tRecv-Q\tSend-Q\tLocal Address:Port\tPeer Address:Port" | cat - <(ss -tuln sport = ":$1" | tail -n +2 | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5}') | prettytable 5 green
 }
 
 list_docker_images() {
-    docker images
+    echo -e "REPOSITORY\tTAG\tIMAGE ID\tCREATED\tSIZE" | cat - <(docker images | tail -n +2) | prettytable 5 blue
 }
 
 list_docker_containers() {
-    docker ps
+    echo -e "CONTAINER ID\tIMAGE\tCOMMAND\tCREATED\tSTATUS\tPORTS\tNAMES" | cat - <(docker ps | tail -n +2) | prettytable 7 purple
 }
 
 get_container_info() {
-    docker inspect "$1"
+    docker inspect "$1" | jq -r '.[] | {Id, Name, Image, State: .State.Status, IP: .NetworkSettings.IPAddress, Ports: .NetworkSettings.Ports}' | prettytable 6 yellow
 }
 
 display_nginx_domains() {
-   sudo nginx -T | grep "server_name"
+    echo -e "Server Name" | cat - <(sudo nginx -T | grep "server_name" | awk '{print $2}' | sort | uniq) | prettytable 1 light_blue
 }
 
-display_nginx_domain_inf0() {
-    grep -A 10 -B 10 "server_name $1" /etc/nginx/sites-available/*
+display_nginx_domain_info() {
+    echo "Configuration for domain: $1"
+    grep -A 10 -B 10 "server_name $1" /etc/nginx/sites-available/* | prettytable 1 light_green
 }
 
-list_user() {
-    awk -F':' '{ print $1}' /etc/passwd
+list_users() {
+    echo -e "Username" | cat - <(awk -F':' '{ print $1}' /etc/passwd) | prettytable 1 light_cyan
 }
 
 display_user_last_log_in_time() {
-   lastlog
+    lastlog | prettytable 4 light_purple
 }
 
-
 fetch_user_info() {
-    grep "^$1:" /etc/passwd
+    echo -e "Username\tUID\tGID\tHome\tShell" | cat - <(grep "^$1:" /etc/passwd | awk -F: '{print $1"\t"$3"\t"$4"\t"$6"\t"$7}') | prettytable 5 light_red
 }
 
 display_time_range_info_for_a_particular_date() {
-   local target_date="$1"
+    local start_date="$1"
+    local end_date="$2"
 
-  start_timestamp="$target_date"
-  end_timestamp="$target_date"
-  journalctl --since "$start_timestamp" --until "$end_timestamp" 
+    if [[ -z "$end_date" ]]; then
+        end_date="$start_date"
+    fi
 
+    journalctl --since "$start_date 00:00:00" --until "$end_date 23:59:59" | prettytable 3 yellow
 }
 
 
@@ -187,7 +138,7 @@ Options:
     -d, --docker [NAME]     Display Docker images and containers or specific container info
     -n, --nginx [DOMAIN]    Display Nginx domains and ports or specific domain info
     -u, --users [USERNAME]  List users and their last login times or specific user info
-    -t, --time              Specify a time range (Not Implemented)
+    -t, --time DATE         Display logs for a specific date (format: YYYY-MM-DD)
     -m, --monitor           Enable continuous monitoring mode
     -h, --help              Show this help message"
 }
@@ -196,26 +147,26 @@ monitor() {
     while true; do
         log "Monitoring system activities..."
         log "Active Ports:"
-        get_active_ports >> "$LOG_FILE"
+        display_active_ports >> "$LOG_FILE"
         log "Docker Containers:"
         list_docker_containers >> "$LOG_FILE"
         log "Users and Last Logins:"
-        list_users >> "$LOG_FILE"
+        display_user_last_log_in_time >> "$LOG_FILE"
         sleep 60
     done
 }
 
 main() {
     if [[ $# -eq 0 ]]; then
-      display_help_options
-      exit 1
+        display_help_options
+        exit 1
     fi
 
     while [[ $# -gt 0 ]]; do
         case "$1" in 
             -p|--port)
                 if [[ -z "$2" || "$2" == -* ]]; then
-                 display_active_ports
+                    display_active_ports
                 else
                     get_port_info "$2"
                     shift
@@ -230,12 +181,11 @@ main() {
                     shift
                 fi
                 ;;  
-
             -n|--nginx)
                 if [[ -z "$2" || "$2" == -* ]]; then
                     display_nginx_domains
                 else
-                    display_nginx_domain_inf0 "$2"
+                    display_nginx_domain_info "$2"
                     shift
                 fi
                 ;;  
@@ -244,23 +194,31 @@ main() {
                     list_users
                     display_user_last_log_in_time
                 else
-                    get_user_info "$2"
-                    get_user_login_times "$2"
+                    fetch_user_info "$2"
                     shift
                 fi
                 ;;
             -t|--time)
-                
+                if [[ -z "$2" || "$2" == -* ]]; then
+                    echo "Please provide a date or date range."
+                    exit 1
+                elif [[ -z "$3" || "$3" == -* ]]; then
+                    display_time_range_info_for_a_particular_date "$2"
+                else
+                    display_time_range_info_for_a_particular_date "$2" "$3"
+                    shift
+                fi
+                shift
                 ;;
             -m|--monitor)
                 monitor
                 ;;
             -h|--help)
-                show_help
+                display_help_options
                 ;;
             *)
                 echo "Invalid option: $1"
-                show_help
+                display_help_options
                 exit 1
                 ;;                      
         esac 
